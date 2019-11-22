ce56f089121694ca701cd5c8aba96f5ae0444518
Fred Yang <fred@FreddeMacBook-Pro.local>
2019-11-21 22:40:00 -0500

    emergency

--- donkeycar/parts/emergency_brake.py
+++ donkeycar/parts/emergency_brake.py
@@ -39,7 +39,7 @@ class EmergencyBrake(object):
         # multiply with the sonic speed (34300 cm/s)
         # and divide by 2, because there and back
         distance = (TimeElapsed * 34300) / 2
-        print(distance)
+        print("d: ", distance)
      
         return distance
     
