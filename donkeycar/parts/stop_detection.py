857c7a4601c9c7f02aa633a02f20d01f5812a0b0
Fuhu Xiao <fuhu@umich.edu>
2019-11-21 19:59:46 -0500

    stop

--- donkeycar/parts/stop_detection.py
+++ donkeycar/parts/stop_detection.py
@@ -29,7 +29,7 @@ class StopSignDetector(object):
             print(len(stop_signs), "STOP signs found.")
             for (x, y, w, h) in stop_signs:
                 area = max(w * h, area)
-                print("area: ", area)
+                print("-- Area: ", area)
                 print("x: ", x, " | y: ", y, " | w: ", w, " | h: ", h)
                 
         return area
