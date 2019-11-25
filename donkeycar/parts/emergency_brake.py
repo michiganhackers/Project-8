35d4a1fc10e76b6dd3195a5e76f6b532b7a9fb38
Fred Yang <fred@FreddeMacBook-Pro.local>
2019-11-24 21:58:42 -0500

    o

--- donkeycar/parts/emergency_brake.py
+++ donkeycar/parts/emergency_brake.py
@@ -6,13 +6,15 @@ class EmergencyBrake(object):
     def __init__(self):
         self.throttle_coeff = 1.0
         self.stop_distance = 100
+    
     def judge(self):
         stop = False
         if (distance < self.stop_distance):
             stop = True
         return stop
     
-    def run(self, throttle, distance):
+    def run(self, throttle, dist):
+        distance = dist
         print("EMERGENCY")
         stop = self.judge()
         if (stop == True):
