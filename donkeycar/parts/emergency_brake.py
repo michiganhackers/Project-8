c59c1fa60d9a1c3ec1025e597ac08495623d8fa9
Fred Yang <fred@FreddeMacBook-Pro.local>
2019-11-24 22:00:41 -0500

    o

--- donkeycar/parts/emergency_brake.py
+++ donkeycar/parts/emergency_brake.py
@@ -16,12 +16,12 @@ class EmergencyBrake(object):
     
     def run(self, throttle, dist):
         distance = dist
-        print("EMERGENCY")
         stop = self.judge(distance)
         if (stop == True):
             self.throttle_coeff = 0.0
         else:
             self.throttle_coeff = 1.0
+        print("coeff: ", self.throttle_coeff)
         return throttle * self.throttle_coeff
             
             
